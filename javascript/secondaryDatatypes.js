36d910de48a1487e555e8f226d672a8c7cb0847d
Deepika Rajasundaram <deepika452005@gmail.com>
2024-12-28 10:29:12 +0530

    adding index.html

---
+++ javascript/secondaryDatatypes.js
@@ -0,0 +1,29 @@
+//array example
+array=[10,20,30,[40.5,50.6],"a","KEC",true]
+console.log(array);
+
+//Object example Type 1
+objtype1={
+    "Firstname":"deeps",
+    "Lastname":"speed",
+    "age":20
+}
+console.log(objtype1);
+
+//Type2
+objtype2={}
+objtype2["movie"]="Ironman2"
+objtype2["budget"]="100cr"
+objtype2["boxOffice"]="100cr"
+objtype2["release"]="worldwide"
+console.log(objtype2);
+console.log(objtype2 ["boxOffice"]);
+console.log(objtype2.budget);
+
+
+//Type3
+objtype3=new Object()
+objtype3["phone"]="iphone"
+objtype3['store']="App store"
+objtype3['game']="Suduko"
+console.log(objtype3);
