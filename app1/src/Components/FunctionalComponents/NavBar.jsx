06a4e830315511048268919cf3c4a6a0dd59aa03
Deepika Rajasundaram <deepika452005@gmail.com>
2025-01-06 08:56:04 +0530

    changes made in backend

--- app1/src/Components/FunctionalComponents/NavBar.jsx
+++ app1/src/Components/FunctionalComponents/NavBar.jsx
@@ -7,20 +7,17 @@ const NavBar = () => {
         <header>
             <nav>
                 <ul>
-                <li><Link to="/Login">Login</Link></li>
-                <li><Link to="/Signup">Signup</Link></li>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
                     <li><Link to="/gallery">Gallery</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
+                    <li><Link to="/footer">Footer</Link></li>
                     <li><Link to="use-ref">UseRef</Link></li>
-                    <li><Link to="/form">Form</Link></li>
                     <li><Link to="/UseEffect">UseEffect</Link></li>
-                    <li><Link to="/UseContext">UseContext</Link></li>
-                    <li><Link to="/UseMemo">UseMemo</Link></li>
-                    
-                    
-                   
+                    <li><Link to="/usecontext">UseContext</Link></li>
+                    <li><Link to="/usememo">UseMemo</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/signup">Signup</Link></li>
                 </ul>
             </nav>
         </header>
